978004fd2e3459c1525af6d90284f66aa9c35b96
chependuk16 <chependuk16@gmail.com>
2023-11-16 13:30:24 +0300

    Initial Commit

---
+++ LoginApp16.11.23/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginApp16.11.23
+//
+//  Created by Мак Сим on 16.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
